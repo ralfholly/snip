b5a83138048a6321bd3b4877d5666a225178b86f
Ralf Holly <ralf.holly.ext@zeiss.com>
2019-10-08 11:08:47 +0200

    Use SNIP constanst for max/min values

--- test/snip_tests.cpp
+++ test/snip_tests.cpp
@@ -16,21 +16,6 @@ using namespace testing;
 namespace testing {
 namespace snip {
 
-static const uint8_t uint8_max = (uint8_t)(-1);
-static const uint16_t uint16_max = (uint16_t)(-1);
-static const uint32_t uint32_max = (uint32_t)(-1);
-static const uint64_t uint64_max = (uint64_t)(-1);
-
-static const int8_t int8_max = (int8_t)(((uint8_t)1 << (8 - 1)) - 1);
-static const int16_t int16_max = (int16_t)(((uint16_t)1 << (16 - 1)) - 1);
-static const int32_t int32_max = (int32_t)(((uint32_t)1 << (32 - 1)) - 1);
-static const int64_t int64_max = (int64_t)(((uint64_t)1 << (64 - 1)) - 1);
-
-static const int8_t int8_min = -int8_max - 1;
-static const int16_t int16_min = -int16_max - 1;
-static const int32_t int32_min = -int32_max - 1;
-static const int64_t int64_min = -int64_max - 1;
-
 #pragma GCC diagnostic warning "-Wsign-conversion"
 
 #define ASIZE(a) ( sizeof(a) / sizeof(a[0]) )
@@ -45,10 +30,10 @@ TEST(snip, add_unsigned) {
     { error = 0; uint64_t result = snip_add_uint64(42, 2, &error); EXPECT_EQ(0, error); EXPECT_EQ(44U, result); }
 
     // .. Overflow.
-    { error = 0; snip_add_uint8(1, uint8_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_add_uint16(1, uint16_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_add_uint32(1, uint32_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_add_uint64(1, uint64_max, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_uint8(1, SNIP_UINT_MAX(8), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_uint16(1, SNIP_UINT_MAX(16), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_uint32(1, SNIP_UINT_MAX(32), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_uint64(1, SNIP_UINT_MAX(64), &error); EXPECT_EQ(1, error); }
 }
 
 TEST(snip, add_signed) {
@@ -60,21 +45,21 @@ TEST(snip, add_signed) {
     { error = 0; int32_t result = snip_add_int32(-2, 2, &error); EXPECT_EQ(0, error); EXPECT_EQ(0, result); }
     { error = 0; int64_t result = snip_add_int64(-2, 2, &error); EXPECT_EQ(0, error); EXPECT_EQ(0, result); }
 
-    { error = 0; int8_t result = snip_add_int8(int8_min, 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(int8_min + 1, result); }
-    { error = 0; int16_t result = snip_add_int16(int16_min, 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(int16_min + 1, result); }
-    { error = 0; int32_t result = snip_add_int32(int32_min, 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(int32_min + 1, result); }
-    { error = 0; int64_t result = snip_add_int64(int64_min, 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(int64_min + 1, result); }
+    { error = 0; int8_t result = snip_add_int8(SNIP_INT_MIN(8), 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_INT_MIN(8) + 1, result); }
+    { error = 0; int16_t result = snip_add_int16(SNIP_INT_MIN(16), 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_INT_MIN(16) + 1, result); }
+    { error = 0; int32_t result = snip_add_int32(SNIP_INT_MIN(32), 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_INT_MIN(32) + 1, result); }
+    { error = 0; int64_t result = snip_add_int64(SNIP_INT_MIN(64), 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_INT_MIN(64) + 1, result); }
 
     // .. Overflow
-    { error = 0; snip_add_int8(int8_max, 1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_add_int16(int16_max, 1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_add_int32(int32_max, 1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_add_int64(int64_max, 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_int8(SNIP_INT_MAX(8), 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_int16(SNIP_INT_MAX(16), 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_int32(SNIP_INT_MAX(32), 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_int64(SNIP_INT_MAX(64), 1, &error); EXPECT_EQ(1, error); }
 
-    { error = 0; snip_add_int8(int8_min, -1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_add_int16(int16_min, -1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_add_int32(int32_min, -1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_add_int64(int64_min, -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_int8(SNIP_INT_MIN(8), -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_int16(SNIP_INT_MIN(16), -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_int32(SNIP_INT_MIN(32), -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_add_int64(SNIP_INT_MIN(64), -1, &error); EXPECT_EQ(1, error); }
 }
 
 TEST(snip, sub_unsigned) {
@@ -102,21 +87,21 @@ TEST(snip, sub_signed) {
     { error = 0; int32_t result = snip_sub_int32(3, 10, &error); EXPECT_EQ(0, error); EXPECT_EQ(-7, result); }
     { error = 0; int64_t result = snip_sub_int64(3, 10, &error); EXPECT_EQ(0, error); EXPECT_EQ(-7, result); }
 
-    { error = 0; int8_t result = snip_sub_int8(0, int8_max, &error); EXPECT_EQ(0, error); EXPECT_EQ(-int8_max, result); }
-    { error = 0; int16_t result = snip_sub_int16(0, int16_max, &error); EXPECT_EQ(0, error); EXPECT_EQ(-int16_max, result); }
-    { error = 0; int32_t result = snip_sub_int32(0, int32_max, &error); EXPECT_EQ(0, error); EXPECT_EQ(-int32_max, result); }
-    { error = 0; int64_t result = snip_sub_int64(0, int64_max, &error); EXPECT_EQ(0, error); EXPECT_EQ(-int64_max, result); }
+    { error = 0; int8_t result = snip_sub_int8(0, SNIP_INT_MAX(8), &error); EXPECT_EQ(0, error); EXPECT_EQ(-SNIP_INT_MAX(8), result); }
+    { error = 0; int16_t result = snip_sub_int16(0, SNIP_INT_MAX(16), &error); EXPECT_EQ(0, error); EXPECT_EQ(-SNIP_INT_MAX(16), result); }
+    { error = 0; int32_t result = snip_sub_int32(0, SNIP_INT_MAX(32), &error); EXPECT_EQ(0, error); EXPECT_EQ(-SNIP_INT_MAX(32), result); }
+    { error = 0; int64_t result = snip_sub_int64(0, SNIP_INT_MAX(64), &error); EXPECT_EQ(0, error); EXPECT_EQ(-SNIP_INT_MAX(64), result); }
 
     // ... Overflow.
-    { error = 0; snip_sub_int8(int8_min, 1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_sub_int16(int16_min, 1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_sub_int32(int32_min, 1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_sub_int64(int64_min, 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_sub_int8(SNIP_INT_MIN(8), 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_sub_int16(SNIP_INT_MIN(16), 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_sub_int32(SNIP_INT_MIN(32), 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_sub_int64(SNIP_INT_MIN(64), 1, &error); EXPECT_EQ(1, error); }
 
-    { error = 0; snip_sub_int8(int8_min, int8_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_sub_int16(int16_min, int16_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_sub_int32(int32_min, int32_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_sub_int64(int64_min, int64_max, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_sub_int8(SNIP_INT_MIN(8), SNIP_INT_MAX(8), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_sub_int16(SNIP_INT_MIN(16), SNIP_INT_MAX(16), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_sub_int32(SNIP_INT_MIN(32), SNIP_INT_MAX(32), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_sub_int64(SNIP_INT_MIN(64), SNIP_INT_MAX(64), &error); EXPECT_EQ(1, error); }
 }
 
 TEST(snip, ensure_error_not_reset) {
@@ -124,7 +109,7 @@ TEST(snip, ensure_error_not_reset) {
 
     error = 0;
     snip_add_int32(17, 4, &error); EXPECT_EQ(0, error);
-    snip_add_int32(int32_max, 1, &error); EXPECT_EQ(1, error);
+    snip_add_int32(SNIP_INT_MAX(32), 1, &error); EXPECT_EQ(1, error);
     snip_add_int32(17, 4, &error); EXPECT_EQ(1, error); // Error still set.
 }
 
@@ -162,26 +147,26 @@ TEST(snip, mul_unsigned) {
     { error = 0; uint32_t result = snip_mul_uint32(10, 0, &error); EXPECT_EQ(0, error); EXPECT_EQ(0U, result); }
     { error = 0; uint64_t result = snip_mul_uint64(10, 0, &error); EXPECT_EQ(0, error); EXPECT_EQ(0U, result); }
 
-    { error = 0; uint8_t result = snip_mul_uint8(1, uint8_max, &error); EXPECT_EQ(0, error); EXPECT_EQ(uint8_max, result); }
-    { error = 0; uint16_t result = snip_mul_uint16(1, uint16_max, &error); EXPECT_EQ(0, error); EXPECT_EQ(uint16_max, result); }
-    { error = 0; uint32_t result = snip_mul_uint32(1, uint32_max, &error); EXPECT_EQ(0, error); EXPECT_EQ(uint32_max, result); }
-    { error = 0; uint64_t result = snip_mul_uint64(1, uint64_max, &error); EXPECT_EQ(0, error); EXPECT_EQ(uint64_max, result); }
+    { error = 0; uint8_t result = snip_mul_uint8(1, SNIP_UINT_MAX(8), &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_UINT_MAX(8), result); }
+    { error = 0; uint16_t result = snip_mul_uint16(1, SNIP_UINT_MAX(16), &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_UINT_MAX(16), result); }
+    { error = 0; uint32_t result = snip_mul_uint32(1, SNIP_UINT_MAX(32), &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_UINT_MAX(32), result); }
+    { error = 0; uint64_t result = snip_mul_uint64(1, SNIP_UINT_MAX(64), &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_UINT_MAX(64), result); }
 
-    { error = 0; uint8_t result = snip_mul_uint8(2, uint8_max / 2, &error); EXPECT_EQ(0, error); EXPECT_EQ((uint8_max / 2) * 2, result); }
-    { error = 0; uint16_t result = snip_mul_uint16(2, uint16_max / 2, &error); EXPECT_EQ(0, error); EXPECT_EQ((uint16_max / 2) * 2, result); }
-    { error = 0; uint32_t result = snip_mul_uint32(2, uint32_max / 2, &error); EXPECT_EQ(0, error); EXPECT_EQ((uint32_max / 2) * 2, result); }
-    { error = 0; uint64_t result = snip_mul_uint64(2, uint64_max / 2, &error); EXPECT_EQ(0, error); EXPECT_EQ((uint64_max / 2) * 2, result); }
+    { error = 0; uint8_t result = snip_mul_uint8(2, SNIP_UINT_MAX(8) / 2, &error); EXPECT_EQ(0, error); EXPECT_EQ((SNIP_UINT_MAX(8) / 2) * 2, result); }
+    { error = 0; uint16_t result = snip_mul_uint16(2, SNIP_UINT_MAX(16) / 2, &error); EXPECT_EQ(0, error); EXPECT_EQ((SNIP_UINT_MAX(16) / 2) * 2, result); }
+    { error = 0; uint32_t result = snip_mul_uint32(2, SNIP_UINT_MAX(32) / 2, &error); EXPECT_EQ(0, error); EXPECT_EQ((SNIP_UINT_MAX(32) / 2) * 2, result); }
+    { error = 0; uint64_t result = snip_mul_uint64(2, SNIP_UINT_MAX(64) / 2, &error); EXPECT_EQ(0, error); EXPECT_EQ((SNIP_UINT_MAX(64) / 2) * 2, result); }
 
     // ... Overflow.
-    { error = 0; snip_mul_uint8(uint8_max, uint8_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_uint16(uint16_max, uint16_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_uint32(uint32_max, uint32_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_uint64(uint64_max, uint64_max, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_uint8(SNIP_UINT_MAX(8), SNIP_UINT_MAX(8), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_uint16(SNIP_UINT_MAX(16), SNIP_UINT_MAX(16), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_uint32(SNIP_UINT_MAX(32), SNIP_UINT_MAX(32), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_uint64(SNIP_UINT_MAX(64), SNIP_UINT_MAX(64), &error); EXPECT_EQ(1, error); }
 
-    { error = 0; snip_mul_uint8(2, (uint8_max / 2) + 1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_uint16(2, (uint16_max / 2) + 1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_uint32(2, (uint32_max / 2) + 1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_uint64(2, (uint64_max / 2) + 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_uint8(2, (SNIP_UINT_MAX(8) / 2) + 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_uint16(2, (SNIP_UINT_MAX(16) / 2) + 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_uint32(2, (SNIP_UINT_MAX(32) / 2) + 1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_uint64(2, (SNIP_UINT_MAX(64) / 2) + 1, &error); EXPECT_EQ(1, error); }
 }
 
 TEST(snip, mul_signed) {
@@ -209,20 +194,20 @@ TEST(snip, mul_signed) {
     { error = 0; int64_t result = snip_mul_int64(-10, -2, &error); EXPECT_EQ(0, error); EXPECT_EQ(20, result); }
 
     // ... Overflow.
-    { error = 0; snip_mul_int8(int8_max, int8_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_int16(int16_max, int16_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_int32(int32_max, int32_max, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_int64(int64_max, int64_max, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int8(SNIP_INT_MAX(8), SNIP_INT_MAX(8), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int16(SNIP_INT_MAX(16), SNIP_INT_MAX(16), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int32(SNIP_INT_MAX(32), SNIP_INT_MAX(32), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int64(SNIP_INT_MAX(64), SNIP_INT_MAX(64), &error); EXPECT_EQ(1, error); }
 
-    { error = 0; snip_mul_int8(int8_min, int8_min, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_int16(int16_min, int16_min, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_int32(int32_min, int32_min, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_int64(int64_min, int64_min, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int8(SNIP_INT_MIN(8), SNIP_INT_MIN(8), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int16(SNIP_INT_MIN(16), SNIP_INT_MIN(16), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int32(SNIP_INT_MIN(32), SNIP_INT_MIN(32), &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int64(SNIP_INT_MIN(64), SNIP_INT_MIN(64), &error); EXPECT_EQ(1, error); }
 
-    { error = 0; snip_mul_int8(int8_min, -1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_int16(int16_min, -1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_int32(int32_min, -1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_mul_int64(int64_min, -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int8(SNIP_INT_MIN(8), -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int16(SNIP_INT_MIN(16), -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int32(SNIP_INT_MIN(32), -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_mul_int64(SNIP_INT_MIN(64), -1, &error); EXPECT_EQ(1, error); }
 }
 
 TEST(snip, div_unsigned) {
@@ -244,10 +229,10 @@ TEST(snip, div_unsigned) {
     { error = 0; uint32_t result = snip_div_uint32(0, 2, &error); EXPECT_EQ(0, error); EXPECT_EQ(0U, result); }
     { error = 0; uint64_t result = snip_div_uint64(0, 2, &error); EXPECT_EQ(0, error); EXPECT_EQ(0U, result); }
 
-    { error = 0; uint8_t result = snip_div_uint8(uint8_max, 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(uint8_max, result); }
-    { error = 0; uint16_t result = snip_div_uint16(uint16_max, 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(uint16_max, result); }
-    { error = 0; uint32_t result = snip_div_uint32(uint32_max, 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(uint32_max, result); }
-    { error = 0; uint64_t result = snip_div_uint64(uint64_max, 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(uint64_max, result); }
+    { error = 0; uint8_t result = snip_div_uint8(SNIP_UINT_MAX(8), 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_UINT_MAX(8), result); }
+    { error = 0; uint16_t result = snip_div_uint16(SNIP_UINT_MAX(16), 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_UINT_MAX(16), result); }
+    { error = 0; uint32_t result = snip_div_uint32(SNIP_UINT_MAX(32), 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_UINT_MAX(32), result); }
+    { error = 0; uint64_t result = snip_div_uint64(SNIP_UINT_MAX(64), 1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_UINT_MAX(64), result); }
 
     // ... Overflow.
     { error = 0; snip_div_int8(10, 0, &error); EXPECT_EQ(1, error); }
@@ -270,21 +255,21 @@ TEST(snip, div_signed) {
     { error = 0; int32_t result = snip_div_int32(11, 2, &error); EXPECT_EQ(0, error); EXPECT_EQ(5, result); }
     { error = 0; int64_t result = snip_div_int64(11, 2, &error); EXPECT_EQ(0, error); EXPECT_EQ(5, result); }
 
-    { error = 0; int8_t result = snip_div_int8(int8_max, -1, &error); EXPECT_EQ(0, error); EXPECT_EQ(int8_min + 1, result); }
-    { error = 0; int16_t result = snip_div_int16(int16_max, -1, &error); EXPECT_EQ(0, error); EXPECT_EQ(int16_min + 1, result); }
-    { error = 0; int32_t result = snip_div_int32(int32_max, -1, &error); EXPECT_EQ(0, error); EXPECT_EQ(int32_min + 1, result); }
-    { error = 0; int64_t result = snip_div_int64(int64_max, -1, &error); EXPECT_EQ(0, error); EXPECT_EQ(int64_min + 1, result); }
+    { error = 0; int8_t result = snip_div_int8(SNIP_INT_MAX(8), -1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_INT_MIN(8) + 1, result); }
+    { error = 0; int16_t result = snip_div_int16(SNIP_INT_MAX(16), -1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_INT_MIN(16) + 1, result); }
+    { error = 0; int32_t result = snip_div_int32(SNIP_INT_MAX(32), -1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_INT_MIN(32) + 1, result); }
+    { error = 0; int64_t result = snip_div_int64(SNIP_INT_MAX(64), -1, &error); EXPECT_EQ(0, error); EXPECT_EQ(SNIP_INT_MIN(64) + 1, result); }
 
     // ... Overflow.
-    { error = 0; snip_div_int8(int8_max, 0, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_div_int16(int16_max, 0, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_div_int32(int32_max, 0, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_div_int64(int64_max, 0, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_div_int8(SNIP_INT_MAX(8), 0, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_div_int16(SNIP_INT_MAX(16), 0, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_div_int32(SNIP_INT_MAX(32), 0, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_div_int64(SNIP_INT_MAX(64), 0, &error); EXPECT_EQ(1, error); }
 
-    { error = 0; snip_div_int8(int8_min, -1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_div_int16(int16_min, -1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_div_int32(int32_min, -1, &error); EXPECT_EQ(1, error); }
-    { error = 0; snip_div_int64(int64_min, -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_div_int8(SNIP_INT_MIN(8), -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_div_int16(SNIP_INT_MIN(16), -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_div_int32(SNIP_INT_MIN(32), -1, &error); EXPECT_EQ(1, error); }
+    { error = 0; snip_div_int64(SNIP_INT_MIN(64), -1, &error); EXPECT_EQ(1, error); }
 }
 
 } // namespace snip
